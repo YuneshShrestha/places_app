9c3a3b02e423bd77532c17f372629c02642ec3a6
Yunesh Shrestha <52854563+YuneshShrestha@users.noreply.github.com>
2023-01-30 14:38:53 +0545

    save image added

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -21,6 +23,9 @@ class _ImageInputState extends State<ImageInput> {
     setState(() {
       image = File(imageFile!.path);
     });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile!.path);
+    final savedImage = await image!.copy('${appDir.path}/$fileName');
   }
 
   @override
