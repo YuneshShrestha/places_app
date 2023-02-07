285dc800da57a60c7e973b84f67f22c8bb5d519f
Yunesh Shrestha <52854563+YuneshShrestha@users.noreply.github.com>
2023-02-07 22:04:53 +0545

    map feature added

--- lib/widgets/locataion_input.dart
+++ lib/widgets/locataion_input.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/models/place.dart';
 import 'package:location/location.dart';
+import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -10,54 +12,90 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String? _previewImage;
+  double latitude = 23;
+  double lognitude = 80;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUserLocation();
+  }
+
   Future<void> _getCurrentUserLocation() async {
     LocationData userLocation = await Location().getLocation();
-    print("Lat: ${userLocation.latitude} , Long: ${userLocation.longitude} ");
+    latitude = userLocation.latitude!;
+    lognitude = userLocation.longitude!;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey,
-              width: 1.0,
-            ),
-          ),
-          width: double.infinity,
-          height: 170,
-          alignment: Alignment.center,
-          child: _previewImage == null
-              ? const Text("Location has not been selected")
-              : Image.network(
-                  _previewImage!,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
+    return Container(
+      width: double.infinity,
+      height: 400,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+          width: 1.0,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton.icon(
-              onPressed: () {
-                _getCurrentUserLocation();
-              },
-              icon: const Icon(Icons.location_on),
-              label: const Text("Current Location"),
-            ),
-            const SizedBox(
-              width: 10.0,
-            ),
-            TextButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.map),
-              label: const Text("Select on map"),
-            )
-          ],
-        ),
-      ],
+      ),
+      child: OpenStreetMapSearchAndPick(
+          center: LatLong(latitude, lognitude),
+          buttonColor: Colors.blue,
+          buttonText: 'Set Current Location',
+          onPicked: (pickedData) {
+            PlaceLocation(
+              latitude: pickedData.latLong.latitude,
+              longitude: pickedData.latLong.longitude,
+              address: pickedData.address,
+            );
+            /*
+            print(pickedData.latLong.latitude);
+            print(pickedData.latLong.longitude);
+            print(pickedData.address);
+            */
+          }),
     );
+    // return Column(
+    //   children: [
+    //     Container(
+    //       decoration: BoxDecoration(
+    //         border: Border.all(
+    //           color: Colors.grey,
+    //           width: 1.0,
+    //         ),
+    //       ),
+    //       width: double.infinity,
+    //       height: 170,
+    //       alignment: Alignment.center,
+    //       child: _previewImage == null
+    //           ? const Text("Location has not been selected")
+    //           : Image.network(
+    //               _previewImage!,
+    //               width: double.infinity,
+    //               fit: BoxFit.cover,
+    //             ),
+    //     ),
+    //     Row(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         TextButton.icon(
+    //           onPressed: () {
+    //             _getCurrentUserLocation();
+    //           },
+    //           icon: const Icon(Icons.location_on),
+    //           label: const Text("Current Location"),
+    //         ),
+    //         const SizedBox(
+    //           width: 10.0,
+    //         ),
+    //         TextButton.icon(
+    //           onPressed: () {},
+    //           icon: const Icon(Icons.map),
+    //           label: const Text("Select on map"),
+    //         )
+    //       ],
+    //     ),
+    //   ],
+    // );
   }
 }
